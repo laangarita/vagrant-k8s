51c9ade4a3942cf7671f260f3160482a1ec04598
Luis Angarita <laangarita@gmail.com>
2019-05-11 09:23:46 -0400

    add logic to not keep adding strings in configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,11 @@ swapoff -a
 sed -i /swap/s/^/#/ /etc/fstab
 
 # Update /etc/hosts file with your static IPs
-sed -i '2 a 192.168.10.10   kubemstr01' /etc/hosts
-sed -i '3 a 192.168.10.20   kubenode01' /etc/hosts
+grep -q "192.168.10" /etc/hosts
+if [[ $? != 0 ]]; then
+  sed -i '2 a 192.168.10.10   kubemstr01' /etc/hosts
+  sed -i '3 a 192.168.10.20   kubenode01' /etc/hosts
+fi
 
 # Install Apache
 apt-get install -y apache2
@@ -42,7 +45,12 @@ apt-get update -y
 apt-get install -y kubelet kubeadm kubectl
 
 # Update the Kubernetes configuration
-sed -i '/^ExecStart=$/i Environment="cgroup-driver=systemd/cgroup-driver=cgroupfs"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+if grep -Fxq 'Environment="cgroup-driver=systemd/cgroup-driver=cgroupfs"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+then
+  exit 0
+else
+  sed -i '/^ExecStart=$/i Environment="cgroup-driver=systemd/cgroup-driver=cgroupfs"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+fi
 
 # Clean up environment
 apt-get autoremove -y
